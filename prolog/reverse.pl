9475e9bf3dc57fffb9ac81bd7e31e0e7fa1e5e7e
Michael Bannister <github@michaelbannister.co.uk>
2018-08-29 21:21:20 +0100

    Prolog day 2: accumulator-based reverse list

--- prolog/reverse.pl
+++ prolog/reverse.pl
@@ -2,3 +2,9 @@ reverselist([], []).
 % reverselist([Head|[]], [Head|[]]).    NOT REQUIRED
 % reverselist(Reversed, [Head|Tail]) :- append(reverselist(Tail), [Head], Reversed).   NO!
 reverselist(Reversed, [Head|Tail]) :- reverselist(ReversedTail, Tail), append(ReversedTail, [Head], Reversed).
+
+
+reverse2helper([H|T], Acc, Result) :- reverse2helper(T, [H|Acc], Result).
+reverse2helper([], Result, Result).
+
+reverse2(Input, Result) :- reverse2helper(Input, [], Result).

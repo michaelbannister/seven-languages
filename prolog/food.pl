416e5e513b50b1294d8059ce5a2cbdcdc08b371c
Michael Bannister <github@michaelbannister.co.uk>
2018-08-29 21:02:54 +0100

    Prolog day 1: examples from chapter

---
+++ prolog/food.pl
@@ -0,0 +1,12 @@
+food_type(velveeta, cheese).
+food_type(ritz, cracker).
+food_type(spam, meat).
+food_type(sausage, meat).
+food_type(jolt, soda).
+food_type(twinkie, dessert).
+flavor(sweet, dessert).
+flavor(savory, meat).
+flavor(savory, cheese).
+flavor(sweet, soda).
+
+food_flavour(Food, Flavour) :- food_type(Food, Type), flavor(Flavour, Type).
---
+++ prolog/friends.pl
@@ -0,0 +1,6 @@
+likes(wallace, cheese).
+likes(gromit, cheese).
+likes(wendolene, sheep).
+
+friend(X, Y) :- \+(X = Y), likes(X, Z), likes(Y, Z).
+
